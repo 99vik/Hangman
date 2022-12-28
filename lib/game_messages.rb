3e791fc7d57f2bfff775777daf6ddba1d197eafe
Viktor Miric <viktorm47@gmail.com>
2022-12-28 13:34:54 +0100

    Almost finished game, guessing letter actions

--- lib/game_messages.rb
+++ lib/game_messages.rb
@@ -5,6 +5,32 @@ module GameMessages
       puts "Press ENTER to start.".bold
       gets
     end
+
+    def letter_included_msg(letter)
+      puts " #{letter.upcase} is included!".green
+    end
+
+    def letter_not_included_msg(letter)
+      puts " #{letter.upcase} is not included!".red
+    end
+
+    def game_won_msg
+      puts "\nCongratulations, you won the game!".green
+      print "The word was: "
+    end
+
+    def enter_letter_msg
+      print "\nEnter choosen letter:"
+    end
+
+    def letter_already_entered_msg
+      puts "\nLetter already entered, please choose different one."
+    end
+
+    def incorrect_letter_input_msg
+      puts "\nIncorrect input.".red
+      puts "Please enter 1 letter.".red
+    end
   end
   
   # Modify messages for colour, boldnes etc.
--- lib/main.rb
+++ lib/main.rb
@@ -7,17 +7,72 @@ class Game
 
   def initialize
     welcome_msg
+    start_game
+  end
+  
+  def start_game
     initialize_variables
     p @secret_word
     until win? || lose? do
       display_board
+      display_entered_letters
       display_guessing_word
-      gets
+      guess_a_letter(input_a_letter)
+    end
+    if win?
+      game_won
+    else
+      game_lost
+    end
+
+  end
+
+  def game_won
+    game_won_msg
+    puts @secret_word.join('').capitalize
+  end
+
+  def guess_a_letter(letter)
+    if @secret_word.include?(letter)
+      letter_included_msg(letter)
+      push_letter_on_guess_array(letter)
+    else
+      letter_not_included_msg(letter)
+       @number_of_guesses += 1
     end
   end
 
+  def push_letter_on_guess_array(letter)
+    all_indexes_in_sw_with_letter(letter).each { |index| @guessing_word[index] = " #{letter} " }
+  end
+
+  def all_indexes_in_sw_with_letter(letter)
+    @secret_word.each_index.select { |index| @secret_word[index] == letter }
+  end
+
+  def input_a_letter
+    enter_letter_msg
+    letter = gets.strip.downcase
+    if letter.length != 1 || !('a'..'z').to_a.include?(letter)
+      incorrect_letter_input_msg
+      input_a_letter
+    elsif @entered_letters.include?(letter)
+      letter_already_entered_msg
+      input_a_letter
+    else
+      @entered_letters.push(letter)
+      letter
+    end
+  end
+
+  def display_entered_letters
+    print "\nPreviously entered letters:   "
+    puts @entered_letters.join(' , ') 
+  end
+
   def display_guessing_word
-    p @guessing_word.join
+    print "\nSecret word:   "
+    puts @guessing_word.join
   end
 
   def display_board
@@ -28,6 +83,7 @@ class Game
     @secret_word = select_random_word
     @guessing_word = Array.new(@secret_word.length, " _ ")
     @number_of_guesses = 0
+    @entered_letters = []
   end
 
   def select_random_word
@@ -42,10 +98,10 @@ class Game
   end
 
   def win?
-
+    !@guessing_word.include?(' _ ')
   end
 
   def lose?
-
+    @number_of_guesses == 6
   end
 end
